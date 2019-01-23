fba0c5b764dbbe5eea0c13d1625f4ba5cd3a0506
Daniel Sonne Lehnberg <doddyswe@gmail.com>
2019-01-23 13:01:13 +0100

    Updates

---
+++ precache-manifest.efc9d6fbd43eb283aea7f2c97958f459.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0567593056fb1343371c",
+    "url": "/ddd-react-todo-list/static/css/main.8e2ce1e2.chunk.css"
+  },
+  {
+    "revision": "0567593056fb1343371c",
+    "url": "/ddd-react-todo-list/static/js/main.05675930.chunk.js"
+  },
+  {
+    "revision": "2b8b5067774a639dc52c",
+    "url": "/ddd-react-todo-list/static/js/1.2b8b5067.chunk.js"
+  },
+  {
+    "revision": "d66da3137e04d562d6fa",
+    "url": "/ddd-react-todo-list/static/js/runtime~main.d66da313.js"
+  },
+  {
+    "revision": "a089e1c894639b520fe9493e73919e8c",
+    "url": "/ddd-react-todo-list/index.html"
+  }
+];
